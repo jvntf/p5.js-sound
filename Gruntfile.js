71bfbece8169e6e5223329ac24699dafd523d774
Jeevan <jeevanfarias@gmail.com>
2017-07-07 15:35:19 +0200

    fixed problem with creating listener

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,6 +76,7 @@ module.exports = function(grunt) {
             'delay': 'src/delay',
             'effect': 'src/effect',
             'spatializer': 'src/spatializer',
+            'listener3d': 'src/listener3d',
             'filter': 'src/filter',
             'reverb': 'src/reverb',
             'distortion': 'src/distortion',
